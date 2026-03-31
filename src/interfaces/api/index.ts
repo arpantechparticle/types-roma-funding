5958d4ccba1f91f091dcbbb02f1506574938e650
dips048 <dipeshpatel048@gmail.com>
2026-03-31 12:55:16 -0400

    types update 1.0.56

--- src/interfaces/api/index.ts
+++ src/interfaces/api/index.ts
@@ -4,5 +4,5 @@ export {UserApiModel} from './user.api.model';
 export {ContactApiModel} from './contact.api.model';
 export {PersonApiModel} from './person.api.model';
 export {BorrowerApiModel, BusinessPrimaryContactModel} from './borrower.api.model';
-export {LoanApiModel, BorrowerModel, LateFeeConfig, AdditionalInfoModel, LoanUnderwritingDocuments } from './loan.api.model';
+export {LoanApiModel, BorrowerModel, LateFeeConfig, AdditionalInfoModel, LoanUnderwritingDocuments, FeeBreakdownModel } from './loan.api.model';
 export {GpsIntegrationApiModel} from './gps-integration.api.model';
